2d82a2f19724af5f263b9eb237d112a917dc3cb0
George Alan Rufo <georgealanrufo@gmail.com>
2023-09-27 14:44:17 -0300

    Chapter 1 finished

--- code-notes/chapter1/src/main/java/org/introduction/DiscountFunctional.java
+++ code-notes/chapter1/src/main/java/org/introduction/DiscountFunctional.java
@@ -1,7 +1,14 @@
 package org.introduction;
 
+import static org.introduction.Prices.prices;
 public class DiscountFunctional {
   public static void main(String[] args) {
+    final double totalOfDiscountedPrices =
+        prices.stream()
+            .filter(price -> price > 20)
+            .mapToDouble(price -> price * 0.9)
+            .sum();
 
+    System.out.println("Total of discounted prices: " + totalOfDiscountedPrices);
   }
 }
